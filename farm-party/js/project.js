803bcf8d387a12b8bb23c2c11fa5b58bf726e40e
asifo <asifubus@gmail.com>
2015-08-14 16:34:55 -0400

    homework farm party

---
+++ farm-party/js/project.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$("#horse").click(function(){
+		$("#horse").animate({left: "+250px",});
+
+	});
+
+	$("#lamb").click(function(){
+		$("#lamb").animate({right: "+250px",});
+
+	});
+
+	$("#cow").click(function(){
+		$("#cow").animate({height: "+350px",});
+
+	});
+
+});
